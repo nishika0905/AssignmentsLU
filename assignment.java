17663d8c3a17494391103ac15adf129800820715
nishika0905 <60567215+nishika0905@users.noreply.github.com>
2021-09-01 19:15:41 +0530

    DAY 2 Assignment

---
+++ assignment.java
@@ -0,0 +1,24 @@
+/* class Drive {
+    int x;
+
+    void show() {
+        System.out.println("This is drive class");
+    }
+}
+*/
+class Test {
+
+    public static void main(String[] args) {
+
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int sum = a + b;
+
+        /*
+         * for (int i = 0; i < args.length; i++); System.out.print(args[i]);
+         */
+
+        System.out.print("SUM IS: " + sum);
+
+    }
+}
